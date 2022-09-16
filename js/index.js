46e6750b2b36d20a88526646ceb1b1964636bc96
amitbhudiya <bhudiyaamit10@gmail.com>
2022-09-16 15:36:12 +0530

    normal final

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
         //slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 5000,
+        infinite:false,
         arrows:false,
-        infinite:false
  });
     $('.smallsl').slick({
         slidesToShow: 4,
@@ -14,20 +14,44 @@ $(document).ready(function () {
         autoplaySpeed: 5000,
         arrows: false,
     });
-    $('.product').slick({
+    $('.productsl').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         infinite: false,
         //autoplay: true,
         autoplaySpeed: 5000,
-        arrows: false,
-    });
+        arrows: true,
+        prevArrow:'.cartprevarrow',
+        nextArrow:'.cartnextarrow'
+   });
     $('.brandsl').slick({
         slidesToShow: 6,
         slidesToScroll: 2,
         infinite: false,
         //autoplay: true,
         autoplaySpeed: 5000,
+        arrows: true,
+        prevArrow:'.prevarrow',
+        nextArrow:'.nextarrow'
+    });
+    $('.testimonials').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: false,
+        //autoplay: true,
+        //autoplaySpeed: 5000,
         arrows: false,
+        // prevArrow:'.prevarrow',
+        // nextArrow:'.nextarrow'
+    });
+    $('.instasl').slick({
+        slidesToShow: 6,
+        slidesToScroll: 2,
+        infinite: false,
+        //autoplay: true,
+        //autoplaySpeed: 5000,
+        arrows: true,
+        prevArrow:'.instaprevarrow',
+        nextArrow:'.instanextarrow'
     });
 });
